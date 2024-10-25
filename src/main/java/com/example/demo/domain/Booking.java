1150981203d518501a18c77bea22221e00317c78
Michael <michaelkivo977@gmail.com>
2024-10-25 21:07:26 +0300

    Add booking entity

---
+++ src/main/java/com/example/demo/domain/Booking.java
@@ -0,0 +1,70 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "booking")
+public class Booking {
+    private Hotel hotel;
+    private User user;
+    private LocalDate date;
+    private double price;
+    private int duration;
+
+    protected Booking() {}
+
+    public Booking(Hotel hotel, User user, LocalDate date, double price, int duration) {
+        this.hotel = hotel;
+        this.user = user;
+        this.date = date;
+        this.price = price;
+        this.duration = duration;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Column(name = "date", nullable = false)
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Column(name = "price", nullable = false)
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Column(name = "duration", nullable = false)
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
