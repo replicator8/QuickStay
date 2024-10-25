0246c75a0726856d6a27c40acd8ad0dcfd229b63
Michael <michaelkivo977@gmail.com>
2024-10-25 21:42:32 +0300

    Add Address dto

---
+++ src/main/java/com/example/demo/dtos/AddressDto.java
@@ -0,0 +1,47 @@
+package com.example.demo.dtos;
+
+public class AddressDto {
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+
+    public AddressDto(String country, String city, String street, String house) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
