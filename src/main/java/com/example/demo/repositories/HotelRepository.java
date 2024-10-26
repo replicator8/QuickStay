b0b1455fc85814cd04118fc734713984182bf099
Michael <michaelkivo977@gmail.com>
2024-10-26 12:19:36 +0300

    Add methods to Hotel repository

--- src/main/java/com/example/demo/repositories/HotelRepository.java
+++ src/main/java/com/example/demo/repositories/HotelRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.repositories;
 
+import com.example.demo.domain.Room;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface HotelRepository {
     String findByName(String name);
+    String findByAddress(String name);
+    double getRating();
+    List<Room> getAllRooms();
+    Room getRoomById(String uuid);
 }
