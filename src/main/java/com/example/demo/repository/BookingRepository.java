58c4eac7b0924ecd6040a098192a44b3a5307089
Michael <michaelkivo977@gmail.com>
2024-11-09 20:51:59 +0300

    remove unnecessary imports

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Booking;
-import com.example.demo.domain.Hotel;
 import com.example.demo.domain.Room;
 import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
