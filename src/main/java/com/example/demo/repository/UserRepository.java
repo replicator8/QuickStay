f4cbba3de45f52576d420d29393123428d3adea6
Michael <michaelkivo977@gmail.com>
2024-11-09 17:08:25 +0300

    Change user repository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    double getBalance(String uuid);
+    @Query("select u.balance from user u where u.id = :uuid")
+    double getBalanceByUserId(String uuid);
+    @Query("select u.firstName, u.lastName from user u where u.id = :uuid")
+    String getFirstNameAndLastNameByUserId(String uuid);
+    @Query("select u.age from user u where u.id = :uuid")
+    int getAgeByUserId(String uuid);
 }
