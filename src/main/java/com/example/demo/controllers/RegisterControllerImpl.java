2df711f97d3829b3e6b449757605ff05f9c0dd6a
Michael <michaelkivo977@gmail.com>
2024-12-08 12:32:52 +0300

    add index.html

--- src/main/java/com/example/demo/controllers/RegisterControllerImpl.java
+++ src/main/java/com/example/demo/controllers/RegisterControllerImpl.java
@@ -4,11 +4,14 @@ import com.example.demo.service.UserService;
 import com.example.quickstay_contracts.controllers.RegisterController;
 import com.example.quickstay_contracts.viewmodel.UserRegisterViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/register")
 public class RegisterControllerImpl implements RegisterController {
     private UserService userService;
@@ -16,6 +19,12 @@ public class RegisterControllerImpl implements RegisterController {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Register");
+        return "index";
     }
 
     // MARK: ok
