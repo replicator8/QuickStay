1af0238c7b001824b018b7b100083478222be630
Michael <michaelkivo977@gmail.com>
2024-12-09 17:31:08 +0300

    fix bug with parsing localdate in thymeleaf

--- src/main/java/com/example/demo/controllers/BookingControllerImpl.java
+++ src/main/java/com/example/demo/controllers/BookingControllerImpl.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
